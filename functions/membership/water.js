296f1729fbb991abd6c683071f19c2b848aa321e
Shejza <shejzaberisha@gmail.com>
2023-04-04 12:42:41 +0200

    water

--- functions/membership/water.js
+++ functions/membership/water.js
@@ -1,4 +1,4 @@
-const LINK_TO = "https://form.jotform.com/230816160501141"
+const LINK_TO = "https://form.jotform.com/230925864258162"
 export async function onRequestGet(context) {
     
     // Contents of context object
