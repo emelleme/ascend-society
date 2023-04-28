cd7a6b56fd6932bd608f74feddc9ae8c7693e919
toby <lloyd@emelle.me>
2023-04-28 18:46:36 -0400

    beta link

--- functions/beta.js
+++ functions/beta.js
@@ -12,7 +12,10 @@ export async function onRequestGet(context) {
     } = context;
     const url = params.path?.join('/') ?? ''
     // console.log(url)
-    return handleRequest(url);
+    // return handleRequest(url);
+    const destinationURL = `${LINK_TO}`;
+    const statusCode = 301;
+    return Response.redirect(destinationURL, statusCode);
     
     //try asset cdn
     
