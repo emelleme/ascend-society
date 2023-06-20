f0400818a4e6ec9cf3c6a4f8451f112eab2c6444
Shejza <shejzaberisha@gmail.com>
2023-06-20 16:48:23 +0200

    other flavors

---
+++ src/_data/mondayData.js
@@ -0,0 +1,17 @@
+const mondayData = [
+  {
+    id: 1,
+    imageSrc: "https://images.unsplash.com/photo-1596710629144-6f6abf933384?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aGliaXNjdXMlMjB0ZWF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+    title: "Hibiscus",
+    price: "$5.00"
+  },
+   {
+    id: 2,
+    imageSrc: "https://images.unsplash.com/photo-1571934811356-5cc061b6821f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNhbGVuZHVsYSUyMHRlYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    title: "Calendula",
+    price: "$5.00"
+  },
+
+]
+
+module.exports = mondayData;
